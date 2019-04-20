4770f801a9c5caaede57bf2d9e425c7fac0f1b39
Sarah Anderson <saranderson13@gmail.com>
2019-04-20 23:47:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,8 +16,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   else
-    puts "HUH?! SPEAK UP SONNY!"
+    "HUH?! SPEAK UP SONNY!"
   end
 end
